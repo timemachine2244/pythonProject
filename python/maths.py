532f22c6ce6983536f71230f7dcb72ae666cec5f
timemachine2244 <71059925+timemachine2244@users.noreply.github.com>
2021-02-17 23:32:14 +0800

    Initial commit

---
+++ python/maths.py
@@ -0,0 +1,16 @@
+class Operation:
+    def add(num1, num2):
+        total=num1+num2
+        return total
+
+    def difference(num1, num2):
+        minus=num1-num2
+        return minus
+
+    def product(num1, num2):
+        prod=num1*num2
+        return prod
+
+    def division(num1, num2):
+        quotient=num1/num2
+        return quotient
